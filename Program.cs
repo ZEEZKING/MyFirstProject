95459dc4bc82db4976eab1e1627de005a9555d5a
ZEEZKING <abdulazeezadekoya17@gmail.com>
2023-05-24 12:05:41 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Hotel_App.Managers.Implementation;
+using Hotel_App.Menu;
+using Hotel_App.Models.enums;
+
+
+MainMenu mainMenu = new MainMenu();
+mainMenu.Main();
+
+// PaymentManager paymentManager = new();
+
+// paymentManager.MakePayment("0123456789", "a@gmail.com", 23456, RoomType.BronzeRoom);
